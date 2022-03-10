230bcf97b67094d90f9c457766c291d8261f3763
17373355 <rebeccag@buaa.edu.cn>
2022-03-10 23:38:21 +0000

    Environment Manager & Panel

---
+++ Assets/ControlBricks.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Brick2move;
+
+public class ControlBricks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void DeleteBrick()
+    {
+        if (transform.childCount > 0)
+        {
+            Debug.Log("Cleaning map...");
+            while (transform.childCount > 0)
+            {
+                Transform child = transform.GetChild(0);
+                
+            }
+        }
+    }
+}
---
+++ Assets/EnvironmentManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentManager : MonoBehaviour
+{
+    public static int[,,] SpaceMap = new int[20,20,20];
+    public static Vector3 playerLocation = Vector3Int.zero;
+    private Vector3 lastLocation = Vector3Int.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i, j, k;
+        Debug.Log("EnvironmentManager Awaken");
+        for(i=0; i<20; i++)
+        {
+            for(j = 0; j < 20; j++)
+            {
+                for (k = 0; k < 20; k++)
+                {
+                    SpaceMap[i, j, k] = 0;
+                    //Debug.Log(SpaceMap[i,j, k]);
+                }
+            }
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerLocation!= lastLocation)
+        {
+            //Debug.Log("Player are moving with brick!");
+            //Debug.Log(playerLocation);
+            lastLocation = playerLocation;
+        }
+    }
+}
