2caeb8630c2acc5892ba79467cb49319d8d23c0a
17373355 <rebeccag@buaa.edu.cn>
2022-03-19 22:17:11 +0000

    Bomb Add

---
+++ Assets/Samples/Ubiq/0.2.0-alpha.4/Samples/Intro/Scripts/TimerCountdown.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerCountdown : MonoBehaviour
+{
+    public GameObject textDisplay;
+    public GameObject gameOver;
+    private int timeLeft = -1;
+    public bool takingAway = false;
+    private bool stop = false;
+    public GameObject bomb;
+    private int minutesLeft;
+    private int secondsLeft;
+    public Button start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (timeLeft == -1)
+        {
+            textDisplay.GetComponent<Text>().text = "Display Time";
+            return;
+        }
+        minutesLeft = timeLeft / 60;
+        secondsLeft = timeLeft % 60;
+        if (secondsLeft < 10)
+        {
+            textDisplay.GetComponent<Text>().text = "0" + minutesLeft + ":" + "0" + secondsLeft;
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = "0" + minutesLeft + ":" + secondsLeft;
+        }
+    }
+
+    public void set_secondsLeft(int value)
+    {
+        timeLeft = value;
+        Start();
+    }
+
+    public void set_stop(bool bool_value)
+    {
+        stop = bool_value;
+    }
+
+    private void Update()
+    {
+        
+        if (takingAway == false && timeLeft > 0 && stop == false)
+        {
+            StartCoroutine(TimerTake());
+        }
+        if (timeLeft == 0)
+        {
+            bomb.GetComponent<Animator>().Play("attack01");
+            gameOver.GetComponent<Text>().text = "GAME OVER!";
+        }
+    }
+
+    IEnumerator TimerTake()
+    {
+        takingAway = true;
+        yield return new WaitForSeconds(1);
+        timeLeft -= 1;
+        minutesLeft = timeLeft / 60;
+        secondsLeft = timeLeft % 60;
+        if (secondsLeft < 10)
+        {
+            textDisplay.GetComponent<Text>().text = "0" + minutesLeft +":"+ "0" +secondsLeft;
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = "0" + minutesLeft + ":" + secondsLeft;
+        }
+        
+        takingAway = false;
+    }
+}
